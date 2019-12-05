7b864aca3784f538e4ce237376680fb068a5b50b
SydMontague <sydmontague@phoenix-staffel.de>
2019-12-05 01:04:26 +0100

    Add line to portal help

--- src/main/java/de/craftlancer/clstuff/help/CCHelpCommandHandler.java
+++ src/main/java/de/craftlancer/clstuff/help/CCHelpCommandHandler.java
@@ -23,7 +23,8 @@ public class CCHelpCommandHandler extends CommandHandler {
     }
     
     private void portalHelp(CommandSender sender) {
-        sender.sendMessage("Get your first portal by ranking up to citizen rank.");
+        sender.sendMessage("Get your first portal by ranking up to hireling rank. ");
+        sender.sendMessage("Be careful where you place it, since it can't be moved later.");
         sender.sendMessage("Make sure to have enough space for a frame to generate and place down your portal (lectern).");
         sender.sendMessage("Use \"/portal name x\" to name your portal. Keep your portal name to yourself and trusted friends!");
         sender.sendMessage("Place a written book with the portal name you want to travel to in the lectern to use the portal.");
