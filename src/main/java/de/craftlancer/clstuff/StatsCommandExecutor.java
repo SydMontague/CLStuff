ecfed0f1fa2bcd0b3f98b3ed07e4af122c3ac5bb
SydMontague <sydmontague@phoenix-staffel.de>
2020-03-30 04:40:14 +0200

    Allow /stats for other players

--- src/main/java/de/craftlancer/clstuff/StatsCommandExecutor.java
+++ src/main/java/de/craftlancer/clstuff/StatsCommandExecutor.java
@@ -1,5 +1,6 @@
 package de.craftlancer.clstuff;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,14 +13,17 @@ public class StatsCommandExecutor implements CommandExecutor {
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player p = (Player) sender;
-            int playtime = p.getStatistic(Statistic.PLAY_ONE_MINUTE);
-            
-            p.sendMessage("Playtime: " + Utils.ticksToTimeString(playtime));
-        }
+        Player player = null;
         
+        if (args.length == 1)
+            player = Bukkit.getPlayer(args[0]);
+        else if (sender instanceof Player)
+            player = (Player) sender;
+        
+        if (player == null)
+            return false;
+        
+        sender.sendMessage("Playtime: " + Utils.ticksToTimeString(player.getStatistic(Statistic.PLAY_ONE_MINUTE)));
         return true;
     }
-    
 }
