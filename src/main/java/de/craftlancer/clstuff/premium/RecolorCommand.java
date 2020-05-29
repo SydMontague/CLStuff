94cc3232f5a185c945070decd2977d7491c8e13b
SydMontague <sydmontague@phoenix-staffel.de>
2020-05-29 18:26:51 +0200

    Remove debug output

--- src/main/java/de/craftlancer/clstuff/premium/RecolorCommand.java
+++ src/main/java/de/craftlancer/clstuff/premium/RecolorCommand.java
@@ -1,4 +1,5 @@
 package de.craftlancer.clstuff.premium;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -30,57 +31,71 @@ import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import de.craftlancer.core.Utils;
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
- 
+
 public class RecolorCommand implements TabExecutor, Listener {
-   
+    
     private final List<String> dyableItems = Arrays.asList("CONCRETE", "BED", "TERRACOTTA", "GLASS", "WOOL", "CARPET");
-    private final List<String> dyeNames = Arrays.asList("Black", "Yellow", "Orange", "Green", "Blue", "Lime",
-            "Gray", "Light_Gray", "White", "Red", "Magenta", "Purple", "Brown",
-            "Pink", "Cyan", "Light_Blue");
-   
+    private final List<String> dyeNames = Arrays.asList("Black",
+                                                        "Yellow",
+                                                        "Orange",
+                                                        "Green",
+                                                        "Blue",
+                                                        "Lime",
+                                                        "Gray",
+                                                        "Light_Gray",
+                                                        "White",
+                                                        "Red",
+                                                        "Magenta",
+                                                        "Purple",
+                                                        "Brown",
+                                                        "Pink",
+                                                        "Cyan",
+                                                        "Light_Blue");
+    
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if (!(commandSender instanceof Player))
             return false;
-       
+        
         Player player = (Player) commandSender;
-       
-        //Check for permission
+        
+        // Check for permission
         if (!player.hasPermission("clstuff.recolor")) {
             player.sendMessage("§f[§4Craft§fCitizen]§c You do not have permission to use this command.");
             return true;
         }
-       
+        
         if (args.length == 0) {
             player.sendMessage("§f[§4Craft§fCitizen]§e You must specify a color!");
             return true;
         }
-       
-        //Is the player holding an item that can be dyed
+        
+        // Is the player holding an item that can be dyed
         String itemName = player.getInventory().getItemInMainHand().getType().name();
         if (dyableItems.stream().noneMatch(itemName::contains)) {
             player.sendMessage("§f[§4Craft§fCitizen]§e You are not holding an item that is dyeable!");
             return true;
         }
-       
-        //If the material isn't valid, return
+        
+        // If the material isn't valid, return
         Material dye;
         try {
             dye = Material.valueOf(args[0].toUpperCase() + "_DYE");
-        } catch (IllegalArgumentException e) {
+        }
+        catch (IllegalArgumentException e) {
             player.sendMessage("§f[§4Craft§fCitizen]§e You must enter a valid color! Use tab complete to help you.");
             return true;
         }
-       
+        
         ItemStack item = player.getInventory().getItemInMainHand().clone();
-       
+        
         Material material = getMaterial(item.getType(), dye);
-       
+        
         if (material == null) {
             player.sendMessage("§f[§4Craft§fCitizen]§e You are not holding an item that is dyeable!");
             return true;
         }
-       
+        
         if (material.equals(item.getType())) {
             player.sendMessage("§f[§4Craft§fCitizen]§e The color you specified is the same as the item's color.");
             return true;
@@ -89,44 +104,38 @@ public class RecolorCommand implements TabExecutor, Listener {
         player.getInventory().setItemInMainHand(item);
         player.updateInventory();
         player.getWorld().playSound(player.getLocation(), Sound.BLOCK_CORAL_BLOCK_HIT, 0.2F, 1F);
-       
+        
         return true;
     }
-   
+    
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if (args.length == 1)
             return Utils.getMatches(args[0], dyeNames);
         return new ArrayList<>();
     }
-   
-   
+    
     @EventHandler(ignoreCancelled = true)
     public void onDyeApply(PlayerInteractEvent event) {
-        System.out.println(1);
         Player player = event.getPlayer();
-       
+        
         if ((event.getAction() != Action.RIGHT_CLICK_BLOCK && event.getAction() != Action.LEFT_CLICK_BLOCK) || !player.isSneaking())
             return;
-
-        System.out.println(2);
+        
         Block block = event.getClickedBlock();
-       
+        
         if (block == null)
             return;
-
-        System.out.println(3);
+        
         if (!player.hasPermission("clstuff.recolor"))
             return;
-
-        System.out.println(4);
+        
         ItemStack item = player.getInventory().getItemInMainHand().clone();
-       
+        
         if (!item.getType().name().contains("DYE"))
             return;
-        System.out.println(5);
-       
-        //If there is a claim, can the player build in it?
+        
+        // If there is a claim, can the player build in it?
         if (GriefPrevention.instance.isEnabled()) {
             Claim claim = null;
             for (Claim c : GriefPrevention.instance.dataStore.getClaims())
@@ -139,27 +148,27 @@ public class RecolorCommand implements TabExecutor, Listener {
             if (claim.allowBuild(player, block.getType()) != null)
                 return;
         }
-       
-        //If there is a WG region, can the player build in it?
+        
+        // If there is a WG region, can the player build in it?
         if (!canPlayerBuildInRegion(block.getLocation(), player))
             return;
-       
+        
         Material material = getMaterial(block.getType(), item.getType());
-       
+        
         if (material == null || block.getType() == material)
             return;
-       
+        
         block.setType(material);
-       
+        
         player.getWorld().playSound(player.getLocation(), Sound.BLOCK_CORAL_BLOCK_HIT, 0.2F, 1F);
-       
+        
     }
-   
+    
     private Material getMaterial(Material item, Material dye) {
         String color = dye.name().replace("_DYE", "");
         String type = item.name();
         String materialString = null;
-       
+        
         if (type.contains("CARPET"))
             materialString = color + "_CARPET";
         else if (type.contains("CONCRETE_POWDER"))
@@ -176,20 +185,20 @@ public class RecolorCommand implements TabExecutor, Listener {
             materialString = color + "_TERRACOTTA";
         else if (type.contains("WOOL"))
             materialString = color + "_WOOL";
-       
+        
         if (materialString == null)
             return null;
-       
+        
         if (!item.name().contains(type))
             return null;
-       
+        
         return Material.valueOf(materialString);
     }
-   
+    
     boolean canPlayerBuildInRegion(Location loc, Player player) {
         if (player.isOp())
             return true;
-       
+        
         RegionManager regions = WorldGuard.getInstance().getPlatform().getRegionContainer().get(new BukkitWorld(loc.getWorld()));
         if (WorldGuard.getInstance() == null || regions == null)
             return true;
@@ -197,17 +206,17 @@ public class RecolorCommand implements TabExecutor, Listener {
         ApplicableRegionSet set = regions.getApplicableRegions(position);
         if (set.size() == 0)
             return true;
-       
+        
         ProtectedRegion topRegion = null;
-       
+        
         for (ProtectedRegion region : set) {
             if (topRegion == null || region.getPriority() > topRegion.getPriority())
                 topRegion = region;
         }
-       
+        
         if (topRegion == null)
             return false;
-       
+        
         return topRegion.isMember(WorldGuardPlugin.inst().wrapPlayer(player));
     }
 }
