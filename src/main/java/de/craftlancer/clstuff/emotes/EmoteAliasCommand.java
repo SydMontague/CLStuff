bdacb16049ac0f2a65eba9cc9e9f36b2eb0b49c6
SizzleMcGrizzle <57070735+SizzleMcGrizzle@users.noreply.github.com>
2020-10-10 12:29:51 -0500

    Emote Changes

--- src/main/java/de/craftlancer/clstuff/emotes/EmoteAliasCommand.java
+++ src/main/java/de/craftlancer/clstuff/emotes/EmoteAliasCommand.java
@@ -1,5 +1,7 @@
 package de.craftlancer.clstuff.emotes;
 
+import de.craftlancer.clstuff.CLStuff;
+import de.craftlancer.core.LambdaRunnable;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -8,15 +10,17 @@ import org.jetbrains.annotations.NotNull;
 public class EmoteAliasCommand extends Command {
     
     private Emote emote;
+    private EmoteManager manager;
     
-    protected EmoteAliasCommand(@NotNull String name, Emote emote) {
+    protected EmoteAliasCommand(@NotNull String name, Emote emote, EmoteManager manager) {
         super(name);
         
         this.emote = emote;
+        this.manager = manager;
     }
     
     @Override
-    public boolean execute(@NotNull CommandSender commandSender, @NotNull String s, @NotNull String[] strings) {
+    public boolean execute(@NotNull CommandSender commandSender, @NotNull String s, @NotNull String[] args) {
         
         if (!(commandSender instanceof Player))
             return false;
@@ -24,11 +28,19 @@ public class EmoteAliasCommand extends Command {
         Player player = (Player) commandSender;
         
         if (!player.hasPermission(emote.getPermission())) {
-            commandSender.sendMessage(EmoteManager.PREFIX + "§You do not have permission to use this command!");
+            commandSender.sendMessage(EmoteManager.PREFIX + "§cYou do not have permission to use this command!");
+            return false;
+        }
+        
+        if (manager.hasCooldown(player.getUniqueId())) {
+            commandSender.sendMessage(EmoteManager.PREFIX + "§cYou must wait to use an emote again!");
             return false;
         }
         
         emote.run(player);
+        
+        manager.addCooldown(player.getUniqueId());
+        new LambdaRunnable(() -> manager.removeCooldown(player.getUniqueId())).runTaskLater(CLStuff.getInstance(), EmoteManager.COOLDOWN);
         
         return true;
     }
